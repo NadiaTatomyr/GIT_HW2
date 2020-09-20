330d6fa80f0b7057f1af26974f1601943dd00fc0
Nadia_Tatomyr <Nadia_Tatomyr@epam.com>
2020-09-20 22:40:55 +0300

    First_commit

---
+++ Task2_Tatomyr/Hello_Tatomyr.cs
@@ -0,0 +1,12 @@
+using System 
+ 
+namespace Hello_Tatomyr 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello <Tatomyr>!"); 
+        } 
+    } 
+} 
